e7c973569696c46e4aed0101f20923266ea9109e
agent_tim <agent_tim@mail.ru>
2023-01-15 08:46:30 +0500

    2023.01.15_1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.example.model.Item;
 import org.example.model.Person;
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -17,8 +18,21 @@ public class App {
         try {
             session.beginTransaction();
 
+            Person person = session.get(Person.class, 1);
+            System.out.println("Получаем человека");
 
             session.getTransaction().commit();
+
+            session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+
+            System.out.println("Другая сессия");
+            person = (Person) session.merge(person);
+            Hibernate.initialize(person.getItems());
+
+            session.getTransaction().commit();
+            System.out.println("После второй сессии");
+            System.out.println(person.getItems());
 
         } finally {
             sessionFactory.close();
